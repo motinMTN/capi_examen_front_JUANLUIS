0b68484b691c26b6931c6e780b38468a10f25182
motinMTN <luigi.motin@gmail.com>
2022-09-09 22:04:07 -0500

    Se modifico el modulo usuarios, se agrego ahi el componente tabla-usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TablaUsuariosComponent } from './components/tabla-usuarios/tabla-usuarios.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { FooterComponent } from './components/partials/footer/footer.component';
 import { LayoutComponent } from './components/layout/layout.component';
-
+import { UsuariosModule } from "./usuarios/usuarios.module";
 import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TablaUsuariosComponent,
     HeaderComponent,
     FooterComponent,
     LayoutComponent
   ],
   imports: [
+    UsuariosModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
